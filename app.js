16581d5607e4ac8afe9c2fc88b517c6edb30cabb
Brandon Tobin <brandontobinbusiness@gmail.com>
2022-11-12 15:03:39 -0600

    got tiles to dispay X on click but not O

--- app.js
+++ app.js
@@ -1,7 +1,39 @@
-const divs = document.querySelectorAll('div')
+// const divs = document.querySelectorAll('div')
 
-document.addEventListener('click', (e) => {
-  if (e.target.matches('div')) {
-    console.log('hi')
+// document.addEventListener('click', (e) => {
+//   if (e.target.matches('div')) {
+//     console.log('hi')
+//   }
+// })
+
+const tiles = document.querySelectorAll('.tile')
+const Player_X = 'X'
+const Player_O = 'O'
+let turn = Player_X
+
+const strike = document.getElementById('strike')
+const gameOverArea = document.getElementById('game-over-area')
+const gameOverText = document.getElementById('game-over-text')
+const playAgain = document.getElementById('play-again')
+
+tiles.forEach((tile) => tile.addEventListener('click', tileClick))
+
+function tileClick(event) {
+  if (gameOverArea.classList.contains('visible')) {
+    return
   }
-})
+  const tile = event.target
+  const tileNumber = tile.dataset.index
+  if (tile.innerText != '') {
+    return
+  }
+  if (turn === Player_X) {
+    tile.innerText = Player_X
+    boardState[tileNumber - 1] = Player_X
+    turn = Player_O
+  } else {
+    tile.innerText = Player_O
+    boardState[tileNumber - 1] = Player_O
+    turn = Player_X
+  }
+}
