5344ca5f262b0f73bb9f2d53aac2a99f99a08354
Brandon Tobin <brandontobinbusiness@gmail.com>
2022-11-12 15:38:47 -0600

    I was able to get X and O to display but it does not end the game and it constantly says Winner is X

--- app.js
+++ app.js
@@ -11,11 +11,14 @@ const Player_X = 'X'
 const Player_O = 'O'
 let turn = Player_X
 
+const boardState = Array(tiles.length)
+boardState.fill(null)
+
 const strike = document.getElementById('strike')
 const gameOverArea = document.getElementById('game-over-area')
 const gameOverText = document.getElementById('game-over-text')
 const playAgain = document.getElementById('play-again')
-
+playAgain.addEventListener('click', startNewGame)
 tiles.forEach((tile) => tile.addEventListener('click', tileClick))
 
 function tileClick(event) {
@@ -43,16 +46,49 @@ function checkWinner() {
   for (const winningCombos of winningCombos) {
     const combo = winningCombos.combo
     const strikeClass = winningCombos.strikeClass
+    const tileValue1 = boardState[combo[0] - 1]
+    const tileValue2 = boardState[combo[1] - 1]
+    const tileValue3 = boardState[combo[2] - 1]
+
+    if (
+      tileValue1 != null &&
+      tileValue1 === tileValue2 &&
+      tileValue1 === tileValue3
+    )
+      strike.classList.add(strikeClass)
+    gameOver(tileValue1)
+    return
   }
+
+  const allTileFilledIn = boardState.every((tile) => tile !== null)
+  if (allTileFilledIn) {
+    gameOver(null)
+  }
+}
+function gameOver(winnderText) {
+  let text = "Cat's game!"
+  if (winnerText != null) {
+    text = `Winner is ${winnerText}!`
+  }
+  gameOverArea.className = 'visible'
+  gameOverText.innerHTML = 'text'
+}
+
+function startNewGame() {
+  strike.className = 'strike'
+  gameOverArea.className = 'hidden'
+  boardState.fill(null)
+  tiles.forEach((tile) => (tile.innerText = ''))
+  turn = Player_X
 }
 
 const winningCombos = [
-  {combo: [1, 2, 3], strikeClass: "strike-row-1"}
-  {combo: [4, 5, 6], strikeClass: "strike-row-2"}
-  {combo: [7, 8, 9], strikeClass: "strike-row-3"}
-  {combo: [1, 4, 7], strikeClass: "strike-column-1"}
-  {combo: [2, 5, 8], strikeClass: "strike-column-2"}
-  {combo: [3, 6, 9], strikeClass: "strike-column-3"}
-  {combo: [1, 5, 9], strikeClass: "strike-diagonal-1"}
-  {combo: [3, 5, 7], strikeClass: "strike-diagonal-2"}
+  { combo: [1, 2, 3], strikeClass: 'strike-row-1' },
+  { combo: [4, 5, 6], strikeClass: 'strike-row-2' },
+  { combo: [7, 8, 9], strikeClass: 'strike-row-3' },
+  { combo: [1, 4, 7], strikeClass: 'strike-column-1' },
+  { combo: [2, 5, 8], strikeClass: 'strike-column-2' },
+  { combo: [3, 6, 9], strikeClass: 'strike-column-3' },
+  { combo: [1, 5, 9], strikeClass: 'strike-diagonal-1' },
+  { combo: [3, 5, 7], strikeClass: 'strike-diagonal-2' }
 ]
